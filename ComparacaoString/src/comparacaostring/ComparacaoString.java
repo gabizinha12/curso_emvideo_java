0a811b5653478b2fbb7e47543fc85cdba4d28fe1
Gabrielle Lima <gabrielle.clima23@gmail.com>
2022-04-29 13:26:03 -0300

    operadores

---
+++ ComparacaoString/src/comparacaostring/ComparacaoString.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package comparacaostring;
+
+/**
+ *
+ * @author Gabizinha
+ */
+public class ComparacaoString {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nome1 = "Gabrielle";
+        String nome2 = "Gabrielle";
+        String nome3 = new String("Gustavo");
+        String res;
+        res = (nome1==nome2)?"igual":"diferente";
+        
+    }
+    
+}
---
+++ OperadorTernario/src/operadorternario/OperadorTernario.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operadorternario;
+
+/**
+ *
+ * @author Gabizinha
+ */
+public class OperadorTernario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n1, n2, r;
+        n1 = 4;
+        n2 = 8;
+        r = (n1>n2)?n1+n2:n1-n2;
+        System.out.println(r);
+        
+    }
+    
+}
